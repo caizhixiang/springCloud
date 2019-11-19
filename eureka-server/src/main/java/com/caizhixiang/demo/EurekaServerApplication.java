84a1f0f1d1540c41fdb35d7d0ffefd58f88280ef
caizhixiang <caizhixiang1989@163.com>
2019-11-19 11:15:03 +0800

    innit

---
+++ eureka-server/src/main/java/com/caizhixiang/demo/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.caizhixiang.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author caizhixiang
+ * @description:
+ * @Date 2019-11-19 11:01
+ * @Version 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
